18fa2a197652bff4aaef9e7e0ea37c0043d81953
Jose Cervantes <jose11cervantes@hotmail.com>
2025-09-07 21:22:32 -0500

    feature: add sourcemap to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -6,11 +6,13 @@ export default {
         {
             file: "dist/simple-js.esm.js",
             format: "esm",
+           sourcemap: true,
         },
         {
             file: "dist/simple-js.umd.js",
             format: "umd",
             name: "SimpleSPA",     
+            sourcemap: true,
             plugins: [terser()]
         }
     ],
